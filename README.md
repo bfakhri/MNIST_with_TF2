70e948aba977aab42d4a98bbee29d1a84df6d41c
Bijan Fakhri <bfakhri@asu.edu>
2020-04-22 13:01:13 -0700

    More verbose

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # MNIST Solver with TF2.0
 Solves MNIST with a very simple fully connected network using the Sequential Model framework from TF Keras. 
 
+This model achieves ~ 91% accuracy in validation. Not bad for a very simple model. 
+
 ## Installation 
 ```
 pip install tensorflow==2.0
--- mnist_solver.py
+++ mnist_solver.py
@@ -1,22 +1,27 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-# Load the dataset
+# Training Params
+batch_size = 32 # How many images to send to model at once
+
+# Load the datasets (training and testing)
 (ds_train, ds_test), info = tfds.load(name='mnist', split=['train', 'test'], with_info=True)
-print(info)
+
+# Get the number of classes in dataset
+num_classes = info.features['label'].num_classes
+
 # Prep the dataset
-def map(x):
+def sample_prepper(x):
     img = tf.cast(x['image'], tf.float32)/255.0
     img_flat = tf.reshape(img, (-1,))
-    label = tf.cast(tf.one_hot(x['label'], 10), tf.float32)
+    label = tf.cast(tf.one_hot(x['label'], num_classes), tf.float32)
     return (img_flat, label)
-ds_train = ds_train.map(map).batch(32)
-ds_test = ds_test.map(map).batch(32)
+ds_train = ds_train.map(sample_prepper).batch(batch_size)
+ds_test = ds_test.map(sample_prepper).batch(batch_size)
 
 # Define the model
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Dense(32, input_shape=(28*28,)))
-# Afterwards, we do automatic shape inference:
 model.add(tf.keras.layers.Dense(32))
 model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
@@ -25,6 +30,8 @@ model.add(tf.keras.layers.Dense(10, activation='softmax'))
 loss_function = tf.keras.losses.CategoricalCrossentropy()
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.00001)
 model.compile(optimizer=optimizer, loss=loss_function, metrics=[tf.keras.metrics.CategoricalAccuracy()])
-model.fit(ds_train, epochs=10, validation_data=ds_test)
+
+# Train the model
+model.fit(ds_train, epochs=30, validation_data=ds_test)
 
 
